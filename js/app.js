84ce99be59da80f91028874e24456acc09da411d
USER <user@USER-no-MacBook-Air.local>
2016-10-21 03:03:17 +0900

    Make base

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDMiEQ1cTohE9NVtpwnN-IgPCMeyw1HC64",
+  authDomain: "twitter-2c966.firebaseapp.com",
+  databaseURL: "https://twitter-2c966.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "817439651469"
+};
+firebase.initializeApp(config);

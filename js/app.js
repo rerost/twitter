8a5b284180028b9bf8ad5fd85d2d640916ec2fa2
rerost <gmailii@yahoo.co.jp>
2016-10-23 13:09:50 +0900

    Finished

--- js/app.js
+++ js/app.js
@@ -1,21 +1,14 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDMiEQ1cTohE9NVtpwnN-IgPCMeyw1HC64",
-  authDomain: "twitter-2c966.firebaseapp.com",
-  databaseURL: "https://twitter-2c966.firebaseio.com",
-  storageBucket: "",
-  messagingSenderId: "817439651469"
-};
-firebase.initializeApp(config);
+var myApp = angular.module("myApp", ["firebase"]);
 
-angular.module('App', [])
-.controller('MainController', ['$scope', function ($scope) {
-  var tweet_database = firebase.database()
-  tweet_database.ref('tweet/' + 'tweet006').push({
-    content: "test",
-    provider_id: 1
-  });
-  tweet_database.ref('tweet/' + 'tweet006').on("value", function(datas) {
-    console.log(datas)
-  });
-}])
+myApp.controller('MyController', ['$scope', '$firebase',
+  function($scope, $firebase) {
+    var ref = new Firebase("https://twitter-2c966.firebaseio.com/tweet");
+    $scope.messages = $firebase(ref).$asArray();
+
+    $("#messageInput").keypress(function (e) {
+      if (e.keyCode == 13) {
+        ref.push({content : $('#messageInput').val(), provider_id : 1})
+      }
+    })
+  }
+]);
